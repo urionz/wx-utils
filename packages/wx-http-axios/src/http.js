75ca79154b278aeb1205d133f51f8a7ebfd867c6
张雨仁 <urionz@urionz.com>
2018-03-10 14:13:47 +0800

    fix copy

--- packages/wx-http-axios/src/http.js
+++ packages/wx-http-axios/src/http.js
@@ -40,49 +40,49 @@ const RequestMQ = {
     }
 }
 
-// function getType(obj){
-//    //tostring会返回对应不同的标签的构造函数
-//    const toString = Object.prototype.toString;
-//    let map = {
-//         '[object Boolean]'  : 'boolean', 
-//         '[object Number]'   : 'number', 
-//         '[object String]'   : 'string', 
-//         '[object Function]' : 'function', 
-//         '[object Array]'    : 'array', 
-//         '[object Date]'     : 'date', 
-//         '[object RegExp]'   : 'regExp', 
-//         '[object Undefined]': 'undefined',
-//         '[object Null]'     : 'null', 
-//         '[object Object]'   : 'object'
-//     };
-//     if(obj instanceof Element) {
-//         return 'element';
-//     }
-//     return map[toString.call(obj)];
-// }
+function getType(obj){
+   //tostring会返回对应不同的标签的构造函数
+   const toString = Object.prototype.toString;
+   let map = {
+        '[object Boolean]'  : 'boolean', 
+        '[object Number]'   : 'number', 
+        '[object String]'   : 'string', 
+        '[object Function]' : 'function', 
+        '[object Array]'    : 'array', 
+        '[object Date]'     : 'date', 
+        '[object RegExp]'   : 'regExp', 
+        '[object Undefined]': 'undefined',
+        '[object Null]'     : 'null', 
+        '[object Object]'   : 'object'
+    };
+    if(obj instanceof Element) {
+        return 'element';
+    }
+    return map[toString.call(obj)];
+}
 
-// function deepClone(data){
-//     const type = getType(data);
-//     let obj;
-//     if(type === 'array'){
-//        obj = [];
-//     } else if(type === 'object'){
-//        obj = {};
-//     } else {
-//        //不再具有下一层次
-//        return data;
-//     }
-//     if(type === 'array'){
-//        for(let i = 0, len = data.length; i < len; i++){
-//            obj.push(deepClone(data[i]));
-//        }
-//     } else if(type === 'object'){
-//        for(var key in data){
-//            obj[key] = deepClone(data[key]);
-//        }
-//     }
-//     return obj;
-// }
+function deepClone(data){
+    const type = getType(data);
+    let obj;
+    if(type === 'array'){
+       obj = [];
+    } else if(type === 'object'){
+       obj = {};
+    } else {
+       //不再具有下一层次
+       return data;
+    }
+    if(type === 'array'){
+       for(let i = 0, len = data.length; i < len; i++){
+           obj.push(deepClone(data[i]));
+       }
+    } else if(type === 'object'){
+       for(var key in data){
+           obj[key] = deepClone(data[key]);
+       }
+    }
+    return obj;
+}
 
 export default class {
     configure = {
@@ -146,8 +146,8 @@ export default class {
         if (this.interceptors.request.before != '') {
             this.interceptors.request.before(this)
         }
-        // const cloneConf = deepClone(this.configure.data)
-        data = Object.assign(this.configure.data, data)
+        const cloneConf = deepClone(this.configure.data, data)
+        data = Object.assign(cloneConf, data)
         method = method || this.configure.method
         url = this.configure.baseURL + url
         const param = {
